a7dc56c099a9bf0142c754c88dec5fb26055844f
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-28 10:46:40 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,55 +34,6 @@ class HustleAcademyApp extends StatelessWidget {
   }
 }
 
-// ============================================================
-// MODELS
-// ============================================================
-class AppLesson {
-  final String title;
-  final String body;
-  final String? codeSnippet;
-  final bool hasImage;
-
-  AppLesson({
-    required this.title,
-    required this.body,
-    this.codeSnippet,
-    this.hasImage = false,
-  });
-}
-
-class AppCourse {
-  final String id;
-  final String title;
-  final String description;
-  final String instructor;
-  final String category;
-  final String difficulty;
-  final IconData icon;
-  final Color color;
-  final List<AppLesson> lessons;
-  final String duration;
-
-  AppCourse({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.instructor,
-    required this.category,
-    required this.difficulty,
-    required this.icon,
-    required this.color,
-    required this.lessons,
-    required this.duration,
-  });
-}
-
-class AppCategory {
-  final String name;
-  final IconData icon;
-  final Color color;
-  AppCategory(this.name, this.icon, this.color);
-}
 
 // ============================================================
 // CATEGORIES
